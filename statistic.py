4b2782fb39080bee02cc3e370f40626ad64f35c8
KevinXuxuxu <kevin.xu.fangzhou@gmail.com>
2015-12-07 20:40:34 +0900

    add time distribution

--- statistic.py
+++ statistic.py
@@ -5,6 +5,7 @@ __email__ = "kevin.xu.fangzhou@gmail.com"
 
 import requests
 import json
+import re
 
 def parse(s):
     try:
@@ -23,12 +24,42 @@ class GraderJobs:
 
     def get_correct_rate(self, exp_id=0):
         if exp_id == 0:
-            self.grade = [r['grade'] for r in self.data]
+            grade = [r['grade'] for r in self.data]
         else:
-            self.grade = [r['grade'] for r in filter(lambda x: parse(x['grader_payload'])['exp_id'] == exp_id, self.data)]
+            grade = [r['grade'] for r in filter(lambda x: parse(x['grader_payload'])['exp_id'] == exp_id, self.data)]
+            if len(grade) == 0:
+                raise Exception("exp id not found.")
 
-        mark = [(1 if p > 0 else 0) for p in self.grade]
-        try:
-            return sum(mark) / float(len(mark))
-        except ZeroDivisionError:
-            raise Exception("exp id not found")
+        mark = [(1 if p > 0 else 0) for p in grade]
+        return sum(mark) / float(len(mark))
+
+    def get_time_distr(self, exp_id=0):
+        if exp_id == 0:
+            time = [r['created_at'] for r in self.data]
+        else:
+            time = [r['created_at'] for r in filter(lambda x: parse(x['grader_payload'])['exp_id'] == exp_id, self.data)]
+            if len(time) == 0:
+                raise Exception("exp id not found.")
+
+        time = sorted(time)
+        parsed_time = map(lambda x:re.split('[A-Z\.\:\-]',x), time)
+        res = []
+        current_day = ""
+        current_count = 0
+        for t in parsed_time:
+            if t[1:3] != current_day:
+                if current_day != "":
+                    res += [("%s-%s" %(current_day[0],current_day[1]), current_count)]
+                current_day = t[1:3]
+                current_count = 1
+            else:
+                current_count += 1
+        res += [("%s-%s" %(current_day[0],current_day[1]), current_count)]
+        return res
+
+def main():
+    gd = GraderJobs()
+    print gd.get_time_distr(exp_id=2)
+
+if __name__ == "__main__":
+    main()
